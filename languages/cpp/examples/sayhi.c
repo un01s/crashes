075bbc2173dec90d21b60e7f2473a37fdc7587e9
un01s <96022876+un01s@users.noreply.github.com>
2022-09-09 09:24:21 -0600

    update

--- languages/cpp/examples/sayhi.c
+++ languages/cpp/examples/sayhi.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 
 void sayHello(char *name) {
-  printf("Hello, %s\n", name);
+  printf("Hello, %s!\n", name);
+}
+
+// parameter by value
+void printNumber(int x) {
+  printf("Number: %d\n", x);
+}
+
+// parameter by reference, that is, a pointer to an integer
+void anotherPrint(int *x) {
+  printf("Num: %d\n", *x);
 }
 
 int main() {
-  char name[] = "daniel";
-  sayHello(name);
+  char name[] = "daniel"; // name with [] is an array, inside all char
+  sayHello(name); // name is a pointer, pointing to the start address of the array
+
+  char another[] = "Charles"; // another is an array, inside all char
+  sayHello(another);
+
+  int a = 100;
+  printNumber(a);
+
+  // give the address of the variable of a
+  anotherPrint(&a);
+
   return 0;
 }
 
